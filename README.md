85ef4812cf6bb7cddb2d01f7ea6c75acecc0588e
ivanj09 <xmilleniumij@gmail.com>
2018-09-28 19:28:38 +0700

    Added README.md, validation for email & password

---
+++ README.md
@@ -0,0 +1,22 @@
+#HerB
+
+## Daftar isi
+1. [Tentang HerB](https://github.com/ivanj09/HerB#tentang-herb)
+2. [Anggota Tim](https://github.com/ivanj09/HerB#anggota-tim)
+3. [Tahap Pengembangan](https://github.com/ivanj09/HerB#tahap-pengembangan)
+-----------
+### Tentang HerB
+**HerB** adalah suatu proyek yang digagas oleh salah satu anggota Tim kami yaitu Gabriel Dicky. Rencananya, **HerB** akan berbentuk gelang yang dapat mengukur detak jantung seseorang
+dengan memakai sensor yang diletakkan pada gelang tersebut. Lalu, harapan kami, pengukuran detak jantung tersebut dapat dikirimkan ke perangkat seluler pengguna secara hampir mendekati *real time*
+dengan periodik tertentu. Selanjutnya, data kesehatan pengguna dapat dibagikan ke kerabat terdekat sehingga hal-hal yang tidak diinginkan tentunya dapat ditangani dengan lebih cepat.
+
+### Anggota Tim
+- Ivan Jonathan
+- Gabriel Dicky
+- Shandy Gunawan
+
+### Tahap Pengembangan
+[x] Ide
+[] Pengembangan
+[] Final
+
--- app/src/main/java/com/ivanjt/herb/LoginActivity.java
+++ app/src/main/java/com/ivanjt/herb/LoginActivity.java
@@ -4,8 +4,13 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,22 +24,115 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText mEmailEditText;
     private EditText mPasswordEditText;
+    private Button mSignInWithEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        //Reference object to items in xml
+        //Get current instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        //If user in active state
+        //        if (mAuth.getCurrentUser() != null){
+        //            startActivity(new Intent(this, DashboardActivity.class));
+        //        }
+
+        //Reference object to views in xml
         mEmailEditText = this.findViewById(R.id.et_email_address);
         mPasswordEditText = this.findViewById(R.id.et_password);
+        mSignInWithEmail = this.findViewById(R.id.bt_sign_in_by_email);
+
+        //Set enabled to false
+        mSignInWithEmail.setEnabled(false);
+
+        //Implements setOnEditorActionListener for email
+        mEmailEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_NEXT){
+                    validateEmail(mEmailEditText.getText().toString());
+
+                    if (isEmailValid(mEmailEditText.getText().toString()) && isPassValid(mPasswordEditText.getText().toString())){
+                        mSignInWithEmail.setEnabled(true);
+                    }
+                }
+                return false;
+            }
+        });
+
+        //Implements setOnEditorActionListener for pass
+        mPasswordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE){
+                    validatePass(mPasswordEditText.getText().toString());
+
+                    if (isEmailValid(mEmailEditText.getText().toString()) && isPassValid(mPasswordEditText.getText().toString())){
+                        mSignInWithEmail.setEnabled(true);
+                        mSignInWithEmail.performClick();
+                    }
+                }
+                return false;
+            }
+        });
+
+        //Implements setOnFocusChanged for email
+        mEmailEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b){
+                    validateEmail(mEmailEditText.getText().toString());
+                    if (isEmailValid(mEmailEditText.getText().toString()) && isPassValid(mPasswordEditText.getText().toString())){
+                        mSignInWithEmail.setEnabled(true);
+                    }
+                }
+            }
+        });
+
+        //Implements setOnFocusChanged for pass
+        mPasswordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b){
+                    validatePass(mPasswordEditText.getText().toString());
+
+                    if (isEmailValid(mEmailEditText.getText().toString()) && isPassValid(mPasswordEditText.getText().toString())){
+                        mSignInWithEmail.setEnabled(true);
+                    }
+                }
+            }
+        });
+    }
+
+    private void validateEmail(String s) {
+        if (isEmailValid(s)){
+            mEmailEditText.setError(null);
+        } else {
+            mSignInWithEmail.setEnabled(false);
+            mEmailEditText.setError("Invalid email address, try again!");
+        }
+    }
+    private void validatePass(String s){
+        if (isPassValid(s)){
+            mPasswordEditText.setError(null);
+        } else {
+            mSignInWithEmail.setEnabled(false);
+            mPasswordEditText.setError("Password is too short..");
+        }
+    }
+
+    private boolean isEmailValid(String s) {
+        return Patterns.EMAIL_ADDRESS.matcher(s).matches();
+    }
+
+    private boolean isPassValid(String s){
+        return s.length() >= 8;
     }
 
     public void signInWith(View view) {
         int id = view.getId();
-
-        //Get current instance of FirebaseAuth
-        mAuth = FirebaseAuth.getInstance();
 
         switch (id) {
             case R.id.bt_sign_in_by_email: //If user sign in with email
@@ -51,17 +149,11 @@ public class LoginActivity extends AppCompatActivity {
                 new AsyncTask<Void, Integer, Void>() {
                     @Override
                     protected Void doInBackground(Void... voids) {
-                        try {
-                            Thread.sleep(200);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
                         //Sign in with email and password
                         mAuth.signInWithEmailAndPassword(mEmailEditText.getText().toString(), mPasswordEditText.getText().toString());
 
                         try {
-                            Thread.sleep(200);
+                            Thread.sleep(400);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
