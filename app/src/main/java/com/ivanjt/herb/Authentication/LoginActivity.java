c58e3cd3cbf80cd47aae56fce5cb03bdf68e5449
ivanj09 <xmilleniumij@gmail.com>
2018-09-29 18:51:01 +0700

    Removed validation for sign-in with Email..

--- app/src/main/java/com/ivanjt/herb/Authentication/LoginActivity.java
+++ app/src/main/java/com/ivanjt/herb/Authentication/LoginActivity.java
@@ -1,6 +1,7 @@
-package com.ivanjt.herb;
+package com.ivanjt.herb.Authentication;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.ivanjt.herb.R;
 
 
 /***
@@ -47,88 +49,64 @@ public class LoginActivity extends AppCompatActivity {
         //Set enabled to false
         mSignInWithEmail.setEnabled(false);
 
-        //Implements setOnEditorActionListener for email
+        //Implements OnEditorActionListener for email
         mEmailEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
                 if (i == EditorInfo.IME_ACTION_NEXT){
-                    validateEmail(mEmailEditText.getText().toString());
-
-                    if (isEmailValid(mEmailEditText.getText().toString()) && isPassValid(mPasswordEditText.getText().toString())){
+                    if (Validation.isEmailValid(mEmailEditText.getText().toString()) && Validation.isPassValid(mPasswordEditText.getText().toString())){
                         mSignInWithEmail.setEnabled(true);
+                    } else {
+                        mSignInWithEmail.setEnabled(false);
                     }
                 }
                 return false;
             }
         });
 
-        //Implements setOnEditorActionListener for pass
+        //Implements OnEditorActionListener for pass
         mPasswordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if (i == EditorInfo.IME_ACTION_DONE){
-                    validatePass(mPasswordEditText.getText().toString());
-
-                    if (isEmailValid(mEmailEditText.getText().toString()) && isPassValid(mPasswordEditText.getText().toString())){
+                if (i == EditorInfo.IME_ACTION_GO){
+                    if (Validation.isEmailValid(mEmailEditText.getText().toString()) && Validation.isPassValid(mPasswordEditText.getText().toString())){
                         mSignInWithEmail.setEnabled(true);
                         mSignInWithEmail.performClick();
+                    } else {
+                        mSignInWithEmail.setEnabled(false);
                     }
                 }
                 return false;
             }
         });
 
-        //Implements setOnFocusChanged for email
+        //Implements OnFocusChanged for email
         mEmailEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
                 if (!b){
-                    validateEmail(mEmailEditText.getText().toString());
-                    if (isEmailValid(mEmailEditText.getText().toString()) && isPassValid(mPasswordEditText.getText().toString())){
+                    if (Validation.isEmailValid(mEmailEditText.getText().toString()) && Validation.isPassValid(mPasswordEditText.getText().toString())){
                         mSignInWithEmail.setEnabled(true);
+                    } else {
+                        mSignInWithEmail.setEnabled(false);
                     }
                 }
             }
         });
 
-        //Implements setOnFocusChanged for pass
+        //Implements OnFocusChanged for pass
         mPasswordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
                 if (!b){
-                    validatePass(mPasswordEditText.getText().toString());
-
-                    if (isEmailValid(mEmailEditText.getText().toString()) && isPassValid(mPasswordEditText.getText().toString())){
+                    if (Validation.isEmailValid(mEmailEditText.getText().toString()) && Validation.isPassValid(mPasswordEditText.getText().toString())){
                         mSignInWithEmail.setEnabled(true);
+                    } else {
+                        mSignInWithEmail.setEnabled(false);
                     }
                 }
             }
         });
-    }
-
-    private void validateEmail(String s) {
-        if (isEmailValid(s)){
-            mEmailEditText.setError(null);
-        } else {
-            mSignInWithEmail.setEnabled(false);
-            mEmailEditText.setError("Invalid email address, try again!");
-        }
-    }
-    private void validatePass(String s){
-        if (isPassValid(s)){
-            mPasswordEditText.setError(null);
-        } else {
-            mSignInWithEmail.setEnabled(false);
-            mPasswordEditText.setError("Password is too short..");
-        }
-    }
-
-    private boolean isEmailValid(String s) {
-        return Patterns.EMAIL_ADDRESS.matcher(s).matches();
-    }
-
-    private boolean isPassValid(String s){
-        return s.length() >= 8;
     }
 
     public void signInWith(View view) {
@@ -167,7 +145,7 @@ public class LoginActivity extends AppCompatActivity {
                         if (mAuth.getCurrentUser() != null){
                             Toast.makeText(LoginActivity.this, "Sign In as " + mAuth.getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
                         }
 
                         //Dismiss the progress dialog
@@ -182,5 +160,9 @@ public class LoginActivity extends AppCompatActivity {
             case R.id.bt_sign_in_by_google: //If user sign in with Google Account
                 break;
         }
+    }
+
+    public void signUp(View view) {
+        startActivity(new Intent(this, SignUpActivity.class));
     }
 }
