12fb20c4cc1024d0f7938b21b0ef28afce305ac8
ivanj09 <xmilleniumij@gmail.com>
2018-09-30 11:05:11 +0700

    Added registration function to HerB..

---
+++ app/src/main/java/com/ivanjt/herb/Authentication/Model/User.java
@@ -0,0 +1,10 @@
+package com.ivanjt.herb.Authentication.Model;
+
+public class User {
+    public String name, age;
+
+    public User(String name, String age){
+        this.name = name;
+        this.age = age;
+    }
+}
--- app/src/main/java/com/ivanjt/herb/Authentication/SignUpActivity.java
+++ app/src/main/java/com/ivanjt/herb/Authentication/SignUpActivity.java
@@ -1,9 +1,14 @@
 package com.ivanjt.herb.Authentication;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,15 +16,27 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ivanjt.herb.Authentication.Model.User;
+import com.ivanjt.herb.DashboardActivity;
 import com.ivanjt.herb.R;
 
 public class SignUpActivity extends AppCompatActivity {
+    private static final String TAG = SignUpActivity.class.getSimpleName();
+
     private EditText mEmailEditText;
     private EditText mPasswordEditText;
     private EditText mConfirmPasswordEditText;
     private EditText mAgeEditText;
     private EditText mDisplayNameEditText;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +52,9 @@ public class SignUpActivity extends AppCompatActivity {
         mConfirmPasswordEditText = this.findViewById(R.id.et_confirm_password);
         mAgeEditText = this.findViewById(R.id.et_age);
         mDisplayNameEditText = this.findViewById(R.id.et_display_name);
+
+        //Get instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -46,17 +66,18 @@ public class SignUpActivity extends AppCompatActivity {
         mEmailEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
-                if (!b){
+                if (!b) {
                     Validation.validateEmail(mEmailEditText, mDoneButton);
-                    
+
                     boolean isEmailValid = Validation.isEmailValid(mEmailEditText.getText().toString());
                     boolean isPassValid = Validation.isPassValid(mPasswordEditText.getText().toString());
                     boolean isConfirmPassValid = Validation.isPassValid(mPasswordEditText.getText().toString()) && mConfirmPasswordEditText.getText().length() >= 8;
                     boolean isAgeValid = Validation.isAgeValid(mAgeEditText.getText().toString());
                     boolean isDisplayNameValid = Validation.isDisplayNameValid(mDisplayNameEditText.getText().toString());
 
-                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid){
+                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid) {
                         mDoneButton.setEnabled(true);
+
                     }
                 }
             }
@@ -66,7 +87,7 @@ public class SignUpActivity extends AppCompatActivity {
         mPasswordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
-                if (!b){
+                if (!b) {
                     Validation.validatePass(mPasswordEditText, mDoneButton);
 
                     boolean isEmailValid = Validation.isEmailValid(mEmailEditText.getText().toString());
@@ -75,18 +96,18 @@ public class SignUpActivity extends AppCompatActivity {
                     boolean isAgeValid = Validation.isAgeValid(mAgeEditText.getText().toString());
                     boolean isDisplayNameValid = Validation.isDisplayNameValid(mDisplayNameEditText.getText().toString());
 
-                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid){
+                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid) {
                         mDoneButton.setEnabled(true);
                     }
                 }
             }
         });
-        
+
         //Implements OnFocusChanged for confirm password
         mConfirmPasswordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
-                if (!b){
+                if (!b) {
                     Validation.validateConfirmPass(mPasswordEditText, mConfirmPasswordEditText, mDoneButton);
 
                     boolean isEmailValid = Validation.isEmailValid(mEmailEditText.getText().toString());
@@ -95,18 +116,18 @@ public class SignUpActivity extends AppCompatActivity {
                     boolean isAgeValid = Validation.isAgeValid(mAgeEditText.getText().toString());
                     boolean isDisplayNameValid = Validation.isDisplayNameValid(mDisplayNameEditText.getText().toString());
 
-                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid){
+                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid) {
                         mDoneButton.setEnabled(true);
                     }
                 }
             }
         });
-        
+
         //Implements OnEditorActionListener for email
         mEmailEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if (i == EditorInfo.IME_ACTION_NEXT){
+                if (i == EditorInfo.IME_ACTION_NEXT) {
                     Validation.validateEmail(mEmailEditText, mDoneButton);
 
                     boolean isEmailValid = Validation.isEmailValid(mEmailEditText.getText().toString());
@@ -115,7 +136,7 @@ public class SignUpActivity extends AppCompatActivity {
                     boolean isAgeValid = Validation.isAgeValid(mAgeEditText.getText().toString());
                     boolean isDisplayNameValid = Validation.isDisplayNameValid(mDisplayNameEditText.getText().toString());
 
-                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid){
+                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid) {
                         mDoneButton.setEnabled(true);
                     }
                 }
@@ -127,7 +148,7 @@ public class SignUpActivity extends AppCompatActivity {
         mPasswordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if (i == EditorInfo.IME_ACTION_NEXT){
+                if (i == EditorInfo.IME_ACTION_NEXT) {
                     Validation.validatePass(mPasswordEditText, mDoneButton);
 
                     boolean isEmailValid = Validation.isEmailValid(mEmailEditText.getText().toString());
@@ -136,7 +157,7 @@ public class SignUpActivity extends AppCompatActivity {
                     boolean isAgeValid = Validation.isAgeValid(mAgeEditText.getText().toString());
                     boolean isDisplayNameValid = Validation.isDisplayNameValid(mDisplayNameEditText.getText().toString());
 
-                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid){
+                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid) {
                         mDoneButton.setEnabled(true);
                     }
                 }
@@ -148,7 +169,7 @@ public class SignUpActivity extends AppCompatActivity {
         mConfirmPasswordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if (i == EditorInfo.IME_ACTION_GO){
+                if (i == EditorInfo.IME_ACTION_GO) {
                     Validation.validatePass(mPasswordEditText, mDoneButton);
 
                     boolean isEmailValid = Validation.isEmailValid(mEmailEditText.getText().toString());
@@ -157,8 +178,9 @@ public class SignUpActivity extends AppCompatActivity {
                     boolean isAgeValid = Validation.isAgeValid(mAgeEditText.getText().toString());
                     boolean isDisplayNameValid = Validation.isDisplayNameValid(mDisplayNameEditText.getText().toString());
 
-                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid){
+                    if (isAgeValid && isConfirmPassValid && isPassValid && isEmailValid && isDisplayNameValid) {
                         mDoneButton.setEnabled(true);
+                        registerUser();
                     }
                 }
                 return false;
@@ -171,15 +193,90 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //When all field are validate, user can sign up
-        if (item.isEnabled()){
-            //Sign Up
+        if (item.isEnabled()) {
+            registerUser();
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void registerUser() {
+        //Declare ProgressDialog instance
+        final android.app.ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering your account, please wait...");
+        progressDialog.setProgressNumberFormat(null);
+        progressDialog.setProgressPercentFormat(null);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setProgressStyle(android.app.ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.show();
+
+
+        new AsyncTask<Void, Integer, Boolean>() {
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+                //Sign Up with validated email and pass
+                mAuth.createUserWithEmailAndPassword(mEmailEditText.getText().toString(), mPasswordEditText.getText().toString())
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    User user = new User(
+                                            mDisplayNameEditText.getText().toString(),
+                                            mAgeEditText.getText().toString()
+                                    );
+
+                                    FirebaseDatabase.getInstance().getReference("Users")
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            mAuth.signInWithEmailAndPassword(mEmailEditText.getText().toString(), mPasswordEditText.getText().toString());
+                                        }
+                                    });
+
+                                    try {
+                                        Thread.sleep(400);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        });
+
+                return mAuth.getCurrentUser() != null;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean aBoolean) {
+                super.onPostExecute(aBoolean);
+
+                if (aBoolean) {
+                    Toast.makeText(SignUpActivity.this, "Register successfully!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(SignUpActivity.this, DashboardActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Email address has already been taken, please log in!", Toast.LENGTH_SHORT).show();
+                }
+
+                progressDialog.dismiss();
+            }
+        }.execute();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        //It must be delete in the next commit, only for testing
+        if (mAuth.getCurrentUser() != null)
+            mAuth.signOut();
+    }
+
     private void setCustomActionBar() {
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
             actionBar.setCustomView(R.layout.action_bar);
 
